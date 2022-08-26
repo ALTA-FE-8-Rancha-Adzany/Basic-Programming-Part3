03de0cec613bb3c25cc0831744911a70d68100f8
ray34-cyber <rancha.adzany1@gmail.com>
2022-08-27 03:11:47 +0800

    menambahkan problem2

--- Part5/problem2/problem2.js
+++ Part5/problem2/problem2.js
@@ -1,4 +1,28 @@
-function caesar(offset, input) {}
+function caesar(offset, input) {
+  var arr = [];
+  let text = input.toLowerCase().split("");
+
+  for (let i = 0; i < input.length; i++) {
+    var index = 0;
+    let isi = text[i].charCodeAt(index) + offset;
+    if (isi > 122 && offset >= 26) {
+      isi = 97 + (offset - 1);
+    } else if (isi > 122 && offset < 26) {
+      for (let j = 0; j < offset; j++) {
+        if (offset > 26) {
+          offset -= 25;
+        } else {
+          break;
+        }
+      }
+      isi = 97 + (offset - 1);
+    }
+    let char = String.fromCharCode(isi);
+    arr.push(char);
+  }
+
+  return arr.join("");
+}
 
 console.log(caesar(3, "abc")); // def
 console.log(caesar(2, "alta")); // cnvc
